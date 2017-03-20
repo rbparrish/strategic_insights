4ec7e6ec1cf769b4314e2a7285f136c8ee22a3ea
Rob Parrish <r@treasuredata.com>
2017-03-19 19:32:50 -0700

    crawling of alexa + work in progress

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # strategic_insights
 Pulls together competitive information
+
+call_aws.py - work in progress script for calling alexa services for a particular URL
+url_crawl.py - work in progress script for crawling a particular website for competitive information (changes + subject + new words being used)
---
+++ url_crawl.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import urllib2
+
+"""
+Created on March 19 2016
+
+Intent: Analysis to identify changes
+"""
+
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+soup = BeautifulSoup(html_doc)
+
+
+search_url = 'http://fishbase.se/search.php'
+search_page = urllib2.urlopen(search_url)
